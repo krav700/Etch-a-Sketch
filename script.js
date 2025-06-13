92bfbb5390f037f156bddc779c0f7eb1eadcfd4c
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2025-06-14 01:54:53 +0300

    feat: add button and all make everything work

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const containerOfDivs = document.querySelector("#gridContainer");
+let containerOfDivs = document.querySelector("#gridContainer");
 
 containerOfDivs.style.width = "960px";
 containerOfDivs.style.height = "960px";
@@ -19,4 +19,41 @@ allDivs.forEach(divItem => {
     divItem.addEventListener("mouseenter", () => {
         divItem.style.backgroundColor = "black";
     });
+});
+
+const button = document.querySelector("#gridButton");
+
+button.addEventListener("click", () => {
+    let gridLength = prompt("Please enter the grid lenght (1-100)");
+    
+    if (gridLength > 0 && gridLength < 101)
+    {
+        containerOfDivs.remove();
+        containerOfDivs = document.createElement("div");
+        containerOfDivs.style.width = "960px";
+        containerOfDivs.style.height = "960px";
+        containerOfDivs.setAttribute("id","gridContainer")
+        document.body.appendChild(containerOfDivs);
+
+        for (let i = 0; i < gridLength; i++)
+            {
+                for (let j = 0; j < gridLength; j++)
+                {
+                    const newDiv = document.createElement("div");
+                    newDiv.classList.toggle("gridDivs");
+                    newDiv.style.width = `${960 / gridLength}px`;
+                    newDiv.style.height = `${960 / gridLength}px`;
+                    containerOfDivs.appendChild(newDiv);
+                }
+            }
+            
+            const allDivs = document.querySelectorAll(".gridDivs");
+            
+            allDivs.forEach(divItem => {
+                divItem.addEventListener("mouseenter", () => {
+                    divItem.style.backgroundColor = "black";
+                });
+            });
+
+    }
 });
